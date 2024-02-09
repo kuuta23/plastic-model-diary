73d3bf3f927bc7e8bdec6035530404b7080d6600
kuuta23 <90048768+kuuta23@users.noreply.github.com>
2024-02-09 22:04:30 +0900

    Ad

---
+++ src/Frame/Ad/Ad.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from "./Ad.module.css"
+
+const Ad = () => {
+  return (
+    <div
+    className={styles.Frame}>
+      <a 
+            href="//af.moshimo.com/af/c/click?a_id=4389678&p_id=5834&pc_id=16169&pl_id=74840" 
+            rel="nofollow" 
+            referrerpolicy="no-referrer-when-downgrade">
+                <img 
+                src="//image.moshimo.com/af-img/5511/000000074840.png" 
+                width="320" 
+                height="60" 
+                style={{"border":"none;"}}/>
+            </a>
+            <img 
+            src="//i.moshimo.com/af/i/impression?a_id=4389678&p_id=5834&pc_id=16169&pl_id=74840" 
+            width="1" 
+            height="1" 
+            style={{"border":"none;"}}>
+
+            </img>
+    </div>
+  )
+}
+
+export default Ad
